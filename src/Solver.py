fb19557ee8a46582b27543fc2f0e0dc20b5d0d1b
Alex Gorbunov <sanyo.gorbunov@gmail.com>
2017-06-24 01:04:07 +0200

    Filter flights.

--- src/Solver.py
+++ src/Solver.py
@@ -1,20 +1,28 @@
 import requests
+from random import randint
 
 class Solver:
-    def demo(self):
-        flights = self.get_flights('PRG', 'BRQ','01/09/2017', '01/10/2017')
+    def demo(self, budget, visited_countries):
+        flights = self.get_flights('PRG','01/09/2017', '01/10/2017')
 
         print(len(flights))
 
-        flights_in_budget = self.filter_flights_by_budget(flights, 200)
+        flights_in_budget = list(self.filter_flights_by_budget(flights, budget))
 
-        print(len(list(flights_in_budget)))
+        print(len(flights_in_budget))
 
-        return;
+        flights_in_non_visited_countries = list(self.filter_flights_by_visited_countries(flights_in_budget, visited_countries))
 
-    def get_flights(self, flyFrom, to, dateFrom, dateTo):
+        print(len(flights_in_non_visited_countries))
+
+        flight_winner_id = self.get_flight_winner_id(len(flights_in_non_visited_countries));
+
+        return flights_in_non_visited_countries[flight_winner_id]
+
+
+    def get_flights(self, flyFrom, dateFrom, dateTo):
         r = requests.get(
-            'https://api.skypicker.com/flights?flyFrom={}&to={}&dateFrom={}&dateTo={}&partner=picky'.format(flyFrom, to, dateFrom, dateTo))
+            'https://api.skypicker.com/flights?flyFrom={}&dateFrom={}&dateTo={}&partner=picky'.format(flyFrom, dateFrom, dateTo))
         flights = r.json()['data']
         return flights
 
@@ -22,5 +30,14 @@ class Solver:
         for flight in flights:
             if flight['conversion']['EUR'] < budget: yield flight
 
+    def filter_flights_by_visited_countries(self, flights, visited_contries):
+        for flight in flights:
+            if not flight['countryTo']['name'] in visited_contries: yield flight
 
-Solver().demo()
+    def get_flight_winner_id(self, max):
+        return randint(0, max - 1)
+
+
+
+t = Solver().demo(40, ['Czech Republic', 'Slovakia'])
+print(t['mapIdto'])
